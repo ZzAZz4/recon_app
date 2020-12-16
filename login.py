99cd70d6126a967fdd00ce0490f07ecba2bf4da7
GyLord_69 <esteban.villacorta@utec.edu.pe>
2020-12-16 11:25:11 -0500

    login by code done

--- login.py
+++ login.py
@@ -5,6 +5,9 @@ from kivy.network.urlrequest import UrlRequest
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.factory import Factory
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 import certifi
 import sys
 sys.path.append("/".join(x for x in __file__.split("/")[:-1]))
@@ -20,6 +23,26 @@ Builder.load_file(folder + "/loadingpopup.kv")
 
 # Import the screens used to log the user in
 from signinscreen import SignInScreen
+from facerec_from_webcam_faster import recog_procedure 
+
+Builder.load_string('''
+<CustomPopup>:
+    size_hint: .5, .5
+    auto_dismiss: False
+    background_color: [0, 0, 0, 0.3]
+    opacity: 0.7
+    title: 'Login Error'
+    Button:
+        text: 'Could not log-in'
+        on_press: root.dismiss()
+''')
+
+
+class CustomPopup(Popup):
+    pass
+
+
+
 
 class Login(Screen, EventDispatcher):
   students_codes = []
@@ -30,12 +53,20 @@ class Login(Screen, EventDispatcher):
   #debug = True
   popup = Factory.LoadingPopup()
   popup.background = folder + "/transparent_image.png"
-
+  
   def on_login_success(self, *args):
     print("Logged in successfully", args)
 
   def sign_in(self, code):
-    if code == "201912345":
+    stored_path = os.listdir("./stored/")
+    code_dir = { person for person in stored_path }
+
+    validated = False
+    if code in code_dir:
+      validated = recog_procedure(code)
+    
+    if validated:
       self.login_success = True
     else:
-      print("Cannot log in")
+      p = CustomPopup()
+      p.open()
